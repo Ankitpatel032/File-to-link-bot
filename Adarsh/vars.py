b37feadba330802cf139a3dc10a88f63c81b265c
Ankitpatel032 <133624583+Ankitpatel032@users.noreply.github.com>
2024-04-25 09:50:16 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -33,7 +33,7 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    FQDN = "file-link-ni3v.onrender.com/" 
+    FQDN = "file-to-link-bot-hrt5.onrender.com/" 
     HAS_SSL = True
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
